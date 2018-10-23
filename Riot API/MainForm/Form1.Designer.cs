a896b91e35370d4a8f471a854d44c91bc61c704f
Peter van Kampen <peter.van.kampen@outlook.com>
2018-10-23 18:53:28 +0200

    FIrst window added

--- Riot API/MainForm/Form1.Designer.cs
+++ Riot API/MainForm/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.regionDropdown = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameInput
+            // 
+            this.nameInput.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.nameInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nameInput.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameInput.Location = new System.Drawing.Point(263, 536);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(252, 37);
+            this.nameInput.TabIndex = 0;
+            this.nameInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // regionDropdown
+            // 
+            this.regionDropdown.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.regionDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.regionDropdown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.regionDropdown.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regionDropdown.FormattingEnabled = true;
+            this.regionDropdown.Items.AddRange(new object[] {
+            "EUW",
+            "NA"});
+            this.regionDropdown.Location = new System.Drawing.Point(582, 535);
+            this.regionDropdown.Name = "regionDropdown";
+            this.regionDropdown.Size = new System.Drawing.Size(121, 37);
+            this.regionDropdown.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.label1.Location = new System.Drawing.Point(258, 474);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 58);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Summoner Name              Region\r\n\r\n";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(786, 535);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::MainForm.Properties.Resources.LoL_Icon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1044, 601);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.regionDropdown);
+            this.Controls.Add(this.nameInput);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.ComboBox regionDropdown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Riot API/MainForm/Form1.cs
+++ Riot API/MainForm/Form1.cs
@@ -16,5 +16,10 @@ namespace MainForm
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
